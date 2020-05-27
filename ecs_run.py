5b46c7be6f11f774e39645dce60aae020d11d956
Becca Sadler <beccasadler@MacBook-Air.local>
2020-05-27 16:53:29 +0100

    Addition of a create cluster function

--- ecs_run.py
+++ ecs_run.py
@@ -20,4 +20,31 @@ session = boto3.Session(
 
 ecs_client = boto3.client('ecs')
 
+# Create cluster
 
+def create_cluster(**kwargs):
+    try:
+        response = ecs_client.create_cluster(**kwargs)
+    except botocore.exceptions.ClientError as e: 
+        print(e)
+    else: 
+        print(response)
+
+cluster_details = {
+'clusterName': 'test_cluster',
+'settings' : [
+    {
+        'name': 'containerInsights',
+        'value': 'string'
+    },
+],
+'capacityProviders' : [
+    'string',
+],
+'defaultCapacityProviderStrategy' : [
+    {
+        'capacityProvider': 'string',
+        'weight': 123,
+        'base': 123
+    },
+]}
