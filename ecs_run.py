3a771109e462be2348fff1698d461e0b17389e37
Becky Sadler <rebecca.sadler@ouh.nhs.uk>
2020-07-02 11:36:27 +0100

    Addition of variables from ecs_config file

--- ecs_run.py
+++ ecs_run.py
@@ -2,7 +2,9 @@ import boto3
 import yaml
 import os
 import botocore
+import json
 
+# Loading in Amazon credentials
 amazon_config = yaml.safe_load(open("config.yml"))
 
 amazon_details = amazon_config['amazon']
@@ -11,14 +13,25 @@ access_key = amazon_details['aws_access_key_id']
 secret_key = amazon_details['aws_secret_access_key']
 default_region = amazon_details['aws_default_region']
 
+# Loading in ECS role ARNs, keypair name and security group id for those created in ecs_setup.py 
+ecs_config = yaml.safe_load(open("ecs_config.yml"))
+
+ecs_details = ecs_config['ecs_information']
+
+ecsInstanceRole_arn = ecs_details['ecsInstanceRole_arn']
+ecsS3InputBucketAccess_arn = ecs_details['ecsS3InputBucketAccess_arn']
+ecsS3OutputBucketAccess_arn = ecs_details['ecsS3OutputBucketAccess_arn']
+ecsTaskExecutionRole_arn = ecs_details['ecsTaskExecutionRole_arn']
+keypair_name = ecs_details['keypair_name']
+security_group_ID = ecs_details['security_group_ID']
+
+
 # Creating session (configures credentials and default region)
 session = boto3.Session(
 	aws_access_key_id = access_key,
 	aws_secret_access_key = secret_key,
 	region_name = default_region
 )
-
-ecs_client = boto3.client('ecs')
 
 # Create cluster
 
@@ -28,23 +41,39 @@ def create_cluster(**kwargs):
     except botocore.exceptions.ClientError as e: 
         print(e)
     else: 
+        cluster_name = response['cluster']['clusterName']
+        print(response)
+        return cluster_name
+
+def run_task(**kwargs):
+    try:
+        response = ecs_client.run_task(**kwargs)
+    except botocore.exceptions.ClientError as e:
+        print(e)
+    else:
         print(response)
 
-cluster_details = {
-'clusterName': 'test_cluster',
-'settings' : [
+# Setting up the required clients
+
+
+ecs_client = boto3.client('ecs')
+
+response = client.create_cluster(
+    'clusterName'='t2-xlarge',
+)
+
+
+run_task = {
+'cluster' : cluster_name,
+'count' : 1,
+'launchType' : 'EC2',
+'placementStrategy' : [
     {
-        'name': 'containerInsights',
-        'value': 'string'
+        'type': 'random',
     },
 ],
-'capacityProviders' : [
-    'string',
-],
-'defaultCapacityProviderStrategy' : [
-    {
-        'capacityProvider': 'string',
-        'weight': 123,
-        'base': 123
-    },
-]}
+'referenceId' : 'task_id',
+'taskDefinition' : taskARN
+}
+
+
